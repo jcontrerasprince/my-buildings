3384e102399ad65161db1f0bb94e38f5beb33b14
Jorge Contreras Prince <jcontrerasprince@gmail.com>
2025-12-20 17:54:08 -0500

    Primer commit

---
+++ backend-supabase/supabase/schemas/profiles.sql
@@ -0,0 +1,5 @@
+create table profiles (
+    id bigint,
+    name text,
+    email text
+)
---
+++ frontend-refine/src/contexts/color-mode/index.tsx
@@ -0,0 +1,72 @@
+import { ConfigProvider, theme, ThemeConfig } from "antd";
+import {
+  type PropsWithChildren,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
+type ColorModeContextType = {
+  mode: string;
+  setMode: (mode: string) => void;
+};
+
+export const ColorModeContext = createContext<ColorModeContextType>(
+  {} as ColorModeContextType
+);
+
+export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
+  const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+  const isSystemPreferenceDark = window?.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+  const [mode, setMode] = useState(
+    colorModeFromLocalStorage || systemPreference
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("colorMode", mode);
+  }, [mode]);
+
+  const setColorMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
+
+  const { darkAlgorithm, defaultAlgorithm } = theme;
+
+  const myTheme: ThemeConfig = {
+    token: {
+      fontFamily: "Roboto, system-ui, -apple-system, BlinkMacSystemFont",
+      colorPrimary: '#cb00ddff',
+      borderRadius: 4,
+    },
+  };
+
+  return (
+    <ColorModeContext.Provider
+      value={{
+        setMode: setColorMode,
+        mode,
+      }}
+    >
+      <ConfigProvider
+        // you can change the theme colors here. example: ...RefineThemes.Magenta,
+        theme={{
+          token: myTheme.token,
+          components: myTheme.components,
+          algorithm: mode === "light" ? defaultAlgorithm : darkAlgorithm,
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </ColorModeContext.Provider>
+  );
+};
